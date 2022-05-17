ee7405a08adbb94efe383a1a4bb2fd018e096af1
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-05-17 16:02:36 +0200

    add Dental Tourism page api

--- src/pages/PatientPrivacy.js
+++ src/pages/PatientPrivacy.js
@@ -40,21 +40,7 @@ const PatientPrivacy = () => {
         </nav>
         <h2 className="title">Respecting Patient Privacy</h2>
         <div className="text-light">
-          <VolunteeringBannerRight
-            background="var(--brand_primary)"
-            img="https://res.cloudinary.com/dirbnpgsp/image/upload/v1648770945/pexels-mentatdgt-1024311_fp988a.jpg"
-            paragraphOne="Safeguarding patients privacy and confidentiality in today’s
-          health care environment is increasingly challenging. Advanced
-          technology, new demands in health care, and development, make it
-          more and more difficult to do so."
-            paragraphTwo=" As a best dentist, our connection to our patients depends on
-          understanding and respecting their personal health
-          confidentiality, it is our ethical and legal obligation as health
-          care providers and it is our duty to protect the well being of
-          those who are entrusted to our care."
-            btn="none"
-            link="/"
-          />
+          <VolunteeringBannerRight url="patientPrivacy/respectingPatientPrivacy" />
         </div>
         {items.map((item) => (
           <div key={item.id}>
