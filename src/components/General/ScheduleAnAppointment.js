66f22986039f7ea68bf600a1383d3e89816a75c0
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-05-17 06:42:46 +0200

    add ScheduleAnAppointment api

--- src/components/General/ScheduleAnAppointment.js
+++ src/components/General/ScheduleAnAppointment.js
@@ -1,14 +1,31 @@
+import React, { useEffect, useState } from "react";
+
 const ScheduleAnAppointment = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://test-dental-clinic-api.herokuapp.com/general/scheduleAnAppointment"
+    )
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <div className="ScheduleAnAppointment">
-      <div className="container py-5 text-light d-flex justify-content-between align-items-center flex-wrap">
-        <h1 className="fw-lighter my-3">Schedule An Appointment</h1>
-        <a href="tel:+01231234123" className="my-3">
-          <button className="btn btn-outline-light fs-4">
-            Request Now! <i className="fa fa-calendar-day px-2"></i>
-          </button>
-        </a>
-      </div>
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="container py-5 text-light d-flex justify-content-between align-items-center flex-wrap"
+        >
+          <h1 className="fw-lighter my-3">{item.title}</h1>
+          <a href={`tel:+${item.link}`} className="my-3">
+            <button className="btn btn-outline-light fs-4">
+              Request Now! <i className="fa fa-calendar-day px-2"></i>
+            </button>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
